4e241864ef536bacfcc6121e2b622b6d0f1f585c
Awilda <AwildaCantres@gmail.com>
2015-03-29 01:52:44 -0400

    Innitial Commit

---
+++ js/app.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $('button').click(function() {
+      var toAdd = $("input[name=additems]").val();
+        $('#items').append("<p>"+toAdd+"</p>");
+
+    $("#items").click(function(){
+      $("#items p:first-child").remove();
+
+        });
+    });
+});
